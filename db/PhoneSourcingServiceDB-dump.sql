d9e729b4894995a713f30c6586ed604c436e143f
Alex-Veselov <lexaveselka@mail.ru>
2024-06-04 21:17:29 +0300

    security configured

--- db/PhoneSourcingServiceDB-dump.sql
+++ db/PhoneSourcingServiceDB-dump.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Admins`
+--
+
+DROP TABLE IF EXISTS `Admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Login` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Admins`
+--
+
+LOCK TABLES `Admins` WRITE;
+/*!40000 ALTER TABLE `Admins` DISABLE KEYS */;
+INSERT INTO `Admins` VALUES
+(1,'admin','$2a$12$TvhQSOVdwkyPOBHp6HVzAO1BXl2bfQMW.yz5.esrPH6Vq5en7zjTG','ADMIN'),
+(2,'manager','$2a$12$HRqoXJsOzgeIAgPF6wVbruIZfbQ5.I1AUM2jI3t6DgxpmrYmq.0Hm','MANAGER');
+/*!40000 ALTER TABLE `Admins` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `Orders`
 --
 
@@ -34,7 +63,7 @@ CREATE TABLE `Orders` (
   KEY `PhoneID` (`PhoneID`),
   CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `Users` (`ID`),
   CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`PhoneID`) REFERENCES `Phones` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,14 +73,14 @@ CREATE TABLE `Orders` (
 LOCK TABLES `Orders` WRITE;
 /*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
 INSERT INTO `Orders` VALUES
-(1,1,1,1,999.99,'2023-04-01'),
-(2,2,2,2,1699.98,'2023-04-02'),
-(4,4,4,1,799.99,'2023-04-04'),
 (5,5,5,3,2099.97,'2023-04-05'),
 (6,6,6,1,1199.99,'2023-04-06'),
-(8,8,8,1,499.99,'2023-04-08'),
+(7,7,7,2,1399.98,'2023-04-07'),
 (9,9,9,3,599.97,'2023-04-09'),
-(10,10,10,2,999.98,'2023-04-10');
+(10,10,10,2,999.98,'2023-04-10'),
+(17,4,5,2,2199.98,'2023-05-01'),
+(18,4,5,2,2199.98,'2023-05-01'),
+(19,5,5,1,799.99,'2024-06-01');
 /*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +99,7 @@ CREATE TABLE `Phones` (
   `Description` text DEFAULT NULL,
   `Availability` tinyint(1) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,14 +111,13 @@ LOCK TABLES `Phones` WRITE;
 INSERT INTO `Phones` VALUES
 (1,'Apple','iPhone 13',999.99,'The latest iPhone model',1),
 (2,'Samsung','Galaxy S22',849.99,'Samsung flagship phone',1),
-(3,'Google','Pixel 6',599.99,'Google Pixel 6 smartphone',0),
-(4,'OnePlus','OnePlus 9 Pro',799.99,'OnePlus flagship phone',1),
 (5,'Xiaomi','Mi 11',699.99,'Xiaomi flagship phone',1),
 (6,'Sony','Xperia 1 III',1199.99,'Sony flagship phone',1),
 (7,'Motorola','Edge+',699.99,'Motorola flagship phone',0),
 (8,'LG','Velvet',499.99,'LG mid-range phone',1),
 (9,'Nokia','G20',199.99,'Nokia budget phone',1),
-(10,'Realme','GT Neo 2',499.99,'Realme mid-range phone',1);
+(10,'Realme','GT Neo 2',499.99,'Realme mid-range phone',1),
+(12,'Samsung','Galaxy S21',799.99,'Latest Samsung smartphone with high-end features',1);
 /*!40000 ALTER TABLE `Phones` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,7 +133,7 @@ CREATE TABLE `Users` (
   `Name` varchar(100) NOT NULL,
   `ContactInformation` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,8 +143,6 @@ CREATE TABLE `Users` (
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
 INSERT INTO `Users` VALUES
-(1,'John Doe','john.doe@email.com'),
-(2,'Jane Smith','jane.smith@email.com'),
 (3,'Michael Johnson','michael.johnson@email.com'),
 (4,'Emily Davis','emily.davis@email.com'),
 (5,'David Wilson','david.wilson@email.com'),
@@ -124,7 +150,9 @@ INSERT INTO `Users` VALUES
 (7,'Robert Anderson','robert.anderson@email.com'),
 (8,'Jessica Taylor','jessica.taylor@email.com'),
 (9,'Christopher Brown','christopher.brown@email.com'),
-(10,'Amanda Miller','amanda.miller@email.com');
+(10,'Amanda Miller','amanda.miller@email.com'),
+(12,'John Doe','johndoe@example.com'),
+(14,'John Doe','john.doe@example.com');
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -137,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-20  9:26:21
+-- Dump completed on 2024-06-04 21:14:55
